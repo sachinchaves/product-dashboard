11884c4193540f712e302b98a1a1f5e916df696f
sachinchaves <sachin.chaves1991@gmail.com>
2025-05-07 21:29:51 -0700

    initial project setup with Vue 3 and SCSS
    - Initialized Vue 3 project using Vue CLI
    - Set up basic folder structure

---
+++ src/views/ProductDashboardView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="productTable">
+    <img alt="Storm logo" src="../assets/images/logo.png" />
+    <ProductTable />
+  </div>
+</template>
+
+<script>
+import ProductTable from "../components/ProductTable.vue";
+
+export default {
+  name: "ProductDashboardView",
+  components: {
+    ProductTable,
+  },
+};
+</script>
